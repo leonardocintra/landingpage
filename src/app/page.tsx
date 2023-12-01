6e7c307e1bba6fd2c8ff422e5a07e3d01ade8e6a
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-12-01 19:46:30 -0300

    ✨ feat: add whastapp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Header from "./components/Header";
-import ContactForm from "./components/ContactForm";
 import HighlightSection from "./components/HighlightSection";
 import ServicesSection from "./components/ServicesSection";
 import TestimonialsSection from "./components/TestimonialsSection";
@@ -15,7 +14,6 @@ export default function Home() {
       <NavHeader />
 
       <div className="flex flex-col sm:flex-row p-2 mt-10">
-
         {/* Coluna 1 */}
         <div className="flex-1 sm:pl-40">
           <p className="text-5xl lg:text-6xl font-bold hover:text-sky-200 text-right pr-3">
@@ -33,9 +31,29 @@ export default function Home() {
         </div>
 
         {/* Coluna 2 */}
-        <div className="flex-1 p-4">
-          <h2 className="text-4xl text-center max-w-md text-amber-200">Quero entrar em contato com você</h2>
-          <ContactForm />
+        <div className="flex flex-col flex-1 p-4 items-center justify-center">
+          <h2 className="text-4xl text-center max-w-md text-amber-200">
+            Quero entrar em contato com você
+          </h2>
+
+          <Image
+            src={"/img/gut1.jpeg"}
+            alt="Gustavo Cintra"
+            className="rounded-full pt-5"
+            width={300}
+            height={300}
+          />
+          <div className="mt-5">
+            <Link
+              target="_blank"
+              className="btn btn-outline btn-warning w-full max-w-md text-2xl"
+              href={
+                "https://wa.me/5516991981267?text=Ola, Gostaria de atrair mais clientes para meu negocio usando trafego pago. Vamos negociar ?"
+              }
+            >
+              Enviar mensagem
+            </Link>
+          </div>
         </div>
       </div>
 
